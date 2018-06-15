99aeccb8205032d8a1cb2960bbcc9ec674cf753c
dzambo032 <dzambo032@gmail.com>
2018-06-15 15:20:52 +0200

    added token auth in post user route

--- server/models/user.js
+++ server/models/user.js
@@ -33,7 +33,7 @@ var UserSchema = new mongoose.Schema({
 )
 
 UserSchema.methods.generateAuthToken = function () {
-    var user = this;
+    let user = this;
     var access = 'auth';
     var token = jwt.sign({_id: user._id.toHexString(), access}, 'abc123').toString();
 
