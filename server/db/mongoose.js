b2f72661922248cdead2fac61d0c0a75b8f03781
dzambo032 <dzambo032@gmail.com>
2018-06-18 10:52:08 +0200

    added token auth in post user route

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://dzambo032:Nikola 12345@ds255970.mlab.com:55970/todo-api' || 'mongodb://localhost:27017/TodoApp'); 
+mongoose.connect('mongodb://dzambo032:Nikola 12345@ds263640.mlab.com:63640/todo-api-v2' || 'mongodb://localhost:27017/TodoAppV2'); 
 // mongoose.connect('mongodb://localhost/TodoApp');
 
 module.exports = { mongoose };
